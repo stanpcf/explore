369dafe1c9b9ac2e75412207ea05b1b6c9085f58
stanpcf <fpchappy@163.com>
2018-12-21 12:48:12 +0800

    add readme.md

---
+++ README.md
@@ -0,0 +1,1 @@
+### explore for travis-ci
--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name='travis_explore',
       version='0.1.0',
+      description='explore for travis ci',
       packages=find_packages())
